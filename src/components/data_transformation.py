85a9a64bcf4bffe471e6fcab4518f4e8a2da2ad6
Brajshukl86 <brajshukl4@gmail.com>
2023-11-27 23:38:30 +0530

    Add code in data_transformation.py file

---
+++ src/components/data_transformation.py
@@ -0,0 +1,27 @@
+import sys
+from dataclasses import dataclass
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            pass
+        except:
+            pass
