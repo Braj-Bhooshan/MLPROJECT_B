20a2674158af825eea87c2fa8d137ef46b366074
Brajshukl86 <brajshukl4@gmail.com>
2023-11-28 23:37:09 +0530

    add code in model training file

---
+++ src/components/model_trainer.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+ from src.utils import save_object
+
