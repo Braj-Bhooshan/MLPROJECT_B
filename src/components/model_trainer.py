e7293ea5050f8518b0a502c8e9fd1c779a02cde2
Brajshukl86 <brajshukl4@gmail.com>
2023-11-30 22:56:04 +0530

    hyperparameter tunning done for multiple models

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,7 +47,42 @@ class ModelTrainer:
                 "AdaBoost Classifier":AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            params={
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2',0.5],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Decision tree":{
+                    'criterion':['squared_error','freiedman_mse','absolute_error','poisson'],
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regressin":{},
+                "K-Neighbors Classifier":{
+                    'n_neighbors':[3,5,10],
+                    'algorithm':['auto','ball_tree','kd_tree']
+                },
+                "XGBClassifier":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'n_estimatores':[8,16,32,64,128,256]
+                },
+                "CatBoosting Classifier":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Classifier":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,14 +22,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train,y_train) #model trained
+            gv = GridSearchCV(model,para,cv=3)
+            gv.fit(x_train,y_train)
+
+            model.set_params(**gv.best_params_)
+            model.fit(x_train,y_train)
+
+            # model.fit(x_train,y_train) #model trained
 
             y_train_pred = model.predict(x_train)
 
